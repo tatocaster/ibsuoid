0c50cb3f3667940cc29521a3fa624271ef2bacf3
tatocaster <kutaliatato@gmail.com>
2015-05-16 23:45:31 +0400

    test

--- app/src/main/java/me/tatocaster/ibsuoid/service/TranscriptFetchService.java
+++ app/src/main/java/me/tatocaster/ibsuoid/service/TranscriptFetchService.java
@@ -1,17 +1,22 @@
 package me.tatocaster.ibsuoid.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.android.volley.Response;
 
 import org.json.JSONObject;
 
+import me.tatocaster.ibsuoid.R;
 import me.tatocaster.ibsuoid.network.VolleyClient;
+import me.tatocaster.ibsuoid.ui.MainActivity;
 
 /**
  * Created by tatocaster on 5/5/2015.
@@ -19,6 +24,7 @@ import me.tatocaster.ibsuoid.network.VolleyClient;
 public class TranscriptFetchService extends Service {
     private static final String TAG = "TranscriptFetchService";
     private boolean mRunning;
+    NotificationManager mNotificationManager;
 
     @Override
     public void onCreate() {
@@ -37,7 +43,18 @@ public class TranscriptFetchService extends Service {
             public void onResponse(JSONObject response) {
                 Log.d(TAG, response.toString());
             }
-        }, null, "11200125", prefs.getString("password",""));
+        }, null, "11200125", prefs.getString("password", ""));
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle(R.string.service_notification_label + "")
+                .setContentText(R.string.service_notification_label + "")
+                .setGroup("1")
+                .setOngoing(true)
+//                .setSmallIcon(R.drawable.marker)
+                .setContentIntent(resultPendingIntent);
+        mNotificationManager.notify(10, builder.build());
+
         return START_NOT_STICKY;
     }
 
